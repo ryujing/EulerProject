43a626abf6efefb77f04708ec12edc8cbb724632
takano <ryujing@takanoke.com>
2012-08-12 00:34:06 +0900

    001 comment

--- 003.rb
+++ 003.rb
@@ -1,3 +1,9 @@
+# encoding: utf-8
+#
+# Project Euler - Problem 3
+#  13195 の素因数は 5、7、13、29 である。
+#  600851475143 の素因数のうち最大のものを求めよ。
+#
 i = 600851475143
 j = 2
 
@@ -8,4 +14,5 @@ while i > 1
         j += 1
     end
 end
+
 puts j
