0aae27a96fdf662a47017f5f0b4203be5dad21ee
takano <ryujing@takanoke.com>
2012-08-15 14:35:16 +0900

    激重だったのでリファクタ

--- 009.rb
+++ 009.rb
@@ -7,13 +7,15 @@
 #   例えば, 3² + 4² = 9 + 16 = 25 = 5²である.
 #   a + b + c = 1000となるピタゴラスの三つ組が一つだけ存在する. このa,b,cの積を計算しなさい.
 #
-for a in 1..1000 do
-    for b in 1..1000 do
-        for c in 1..1000 do
-            if (a ** 2 + b ** 2) == c ** 2 && (a + b + c ) == 1000
-                puts "a:"+a.to_s+"  b:"+b.to_s+" c:"+c.to_s+"  total:"+(a * b * c).to_s
-            end
+total = 0
+
+(1..1000).each { |a|
+    ((a + 1)..1000).each { |b|
+        c = 1000 - a - b
+        if (a**2 + b**2) == c**2
+            total = a * b * c
+            break
         end
-   end
-end
-# 単純にこう考えたが、あまりにウンコード（＞＜
+    }
+}
+puts total
